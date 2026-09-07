47088b99e4ce59d356948275fa025de157bafe05
em6race <diorrit@gmail.com>
2026-09-07 17:27:50 +0300

    refactor: rename /smartrestart to /queuerestart

--- src/main/java/com/example/minecord/bot/BotManager.java
+++ src/main/java/com/example/minecord/bot/BotManager.java
@@ -73,7 +73,7 @@ public class BotManager {
                                         new SubcommandData("toggle", "Призупинити/відновити всі авторестарти")
                                 ),
 
-                        Commands.slash("smartrestart", "Поставити одноразовий рестарт у чергу (коли онлайн = 0)")
+                        Commands.slash("queuerestart", "Поставити одноразовий рестарт у чергу (коли онлайн = 0)")
                                 .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)),
                         Commands.slash("stats", "Статистика сервера або гравця")
                                 .addOption(OptionType.STRING, "player", "Нікнейм гравця", false, true),
